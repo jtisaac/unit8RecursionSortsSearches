d1720c4d2c5b72b2863345996ca2cf8be470879d
jtisaac <JTIsaac@student.naperville203.org>
2016-03-11 15:11:31 -0600

    Starting Fibonacci

---
+++ Recursion/ListMethods.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ListMethods
+{
+   public static ArrayList<Integer> makeList(int n)
+   {
+      ArrayList<Integer> tempList = new ArrayList<Integer>(n);
+      int t = n;
+      
+      if (n <= 0)  // The smallest list we can make
+      {
+          return new ArrayList<Integer>();
+          
+      }
+      else        // All other size lists are created here
+      {
+          tempList.add(t);
+          t --;
+          tempList.addAll(ListMethods.makeList(t));
+          
+          
+          
+      }
+      return tempList;
+   }
+   //public static ArrayList<Integer> reverseList(ArrayList<Integer> list)
+   //{
+     //  return
+    //}
+    
+}
